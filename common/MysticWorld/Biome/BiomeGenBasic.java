11d127d26617ce9e54a6641f448d9a22c1a0246b
GotoLink <gotolinkminecraft@gmail.com>
2013-10-26 23:18:36 +0200

    Reorganized biomes

--- common/MysticWorld/Biome/BiomeGenBasic.java
+++ common/MysticWorld/Biome/BiomeGenBasic.java
@@ -1,25 +1,25 @@
 package mysticworld.biome;
 
-import java.util.Random;
-
-import net.minecraft.entity.monster.EntityCreeper;
-import net.minecraft.entity.monster.EntityGhast;
-import net.minecraft.entity.monster.EntityGiantZombie;
 import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.biome.SpawnListEntry;
-import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class BiomeGenBasic extends BiomeGenBase {
-	
-	public BiomeGenBasic(int id, int treesChunk, int grassChunk, int flowerChunk, int reedsChunk, int blockTop, int blockFiller) {
+	public BiomeGenBasic(int id, int blockTop, int blockFiller) {
 		super(id);
 		this.topBlock = (byte) blockTop;
 		this.fillerBlock = (byte) blockFiller;
+	}
+
+	public BiomeGenBasic(int id, int treesChunk, int grassChunk, int flowerChunk, int reedsChunk) {
+		super(id);
 		this.theBiomeDecorator.treesPerChunk = treesChunk;
 		this.theBiomeDecorator.grassPerChunk = grassChunk;
 		this.theBiomeDecorator.flowersPerChunk = flowerChunk;
-		this.theBiomeDecorator.reedsPerChunk = reedsChunk;;
+		this.theBiomeDecorator.reedsPerChunk = reedsChunk;
 	}
-	
-}
 
+	public BiomeGenBasic(int id, int treesChunk, int grassChunk, int flowerChunk, int reedsChunk, int blockTop, int blockFiller) {
+		this(id, treesChunk, grassChunk, flowerChunk, reedsChunk);
+		this.topBlock = (byte) blockTop;
+		this.fillerBlock = (byte) blockFiller;
+	}
+}
--- common/MysticWorld/Biome/BiomeHandler.java
+++ common/MysticWorld/Biome/BiomeHandler.java
@@ -19,7 +19,7 @@ public class BiomeHandler {
 	}
 
 	public static void initializeBiomes() {
-		Mountain = new BiomeGenMountain(23, Block.stone.blockID, Block.stone.blockID).setColor(9286496).setBiomeName("Mountain").setEnableSnow().setTemperatureRainfall(0.05F, 0.8F)
+		Mountain = new BiomeGenBasic(23, Block.stone.blockID, Block.stone.blockID).setColor(9286496).setBiomeName("Mountain").setEnableSnow().setTemperatureRainfall(0.05F, 0.8F)
 				.setMinMaxHeight(0.5F, 1.0F);
 		Tundra = new BiomeGenTundra(24, 0, 0, 0, 0, Block.blockSnow.blockID, Block.blockSnow.blockID).setColor(9286496).setBiomeName("Tundra").setEnableSnow().setTemperatureRainfall(0.05F, 0.8F)
 				.setMinMaxHeight(0.8F, 0.4F);
@@ -27,7 +27,6 @@ public class BiomeHandler {
 		Glacier = new BiomeGenGlacier(26, 0, 0, 0, 0, Block.ice.blockID, Block.ice.blockID).setColor(9474208).setBiomeName("Glacier").setEnableSnow().setTemperatureRainfall(0.05F, 0.8F)
 				.setMinMaxHeight(0.5F, 0.7F);
 		Jungle = new BiomeGenJungle(27).setColor(5470985).setBiomeName("Jungle").func_76733_a(5470985).setTemperatureRainfall(1.2F, 0.9F).setMinMaxHeight(0.2F, 0.4F);
-		;
 	}
 
 	public static void registerBiomes() {
