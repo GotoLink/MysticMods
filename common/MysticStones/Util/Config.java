29c98b8fa4ad9a66ec5eaef65eb63fe1f2d0e434
GotoLink <gotolinkminecraft@gmail.com>
2013-10-26 23:20:10 +0200

    Added config options for each stones rarity

--- common/MysticStones/Util/Config.java
+++ common/MysticStones/Util/Config.java
@@ -7,21 +7,24 @@ import java.util.ArrayList;
 import net.minecraftforge.common.Configuration;
 
 public class Config {
-	public static int PlanksID;
-	public static int StonesID;
-	public static int StoneBricksID;
-	public static int ReinforcedBricksID;
-	public static int Rare;
+	public static final String[] keys = { "anorthosite", "limestone", "marble", "migmatite", "orthogneiss", "slate", "travertine" };
+	public static int planksID;
+	public static int stonesID;
+	public static int stoneBricksID;
+	public static int reinforcedBricksID;
+	public static int[] rare = new int[keys.length];
 	public static List<Integer> allowed = new ArrayList();
 
 	public static void initialize(File file) {
 		Configuration config = new Configuration(file);
 		config.load();
-		PlanksID = config.getBlock("Plank IDs", 509).getInt();
-		StonesID = config.getBlock("Stone IDs", 510).getInt();
-		StoneBricksID = config.getBlock("Stone Brick IDs", 511).getInt();
-		ReinforcedBricksID = config.getBlock("Reinforced Brick IDs", 512).getInt();
-		Rare = config.get("Generation", "Rarity of Stones", 3).getInt();
+		planksID = config.getBlock("Plank IDs", 509).getInt();
+		stonesID = config.getBlock("Stone IDs", 510).getInt();
+		stoneBricksID = config.getBlock("Stone Brick IDs", 511).getInt();
+		reinforcedBricksID = config.getBlock("Reinforced Brick IDs", 512).getInt();
+		for (int i = 0; i < rare.length; i++) {
+			rare[i] = config.get("Generation", "Rarity of " + keys[i], 3).getInt();
+		}
 		for (String id : config.get("Generation", "Allowed Dimension ID", "0,").getString().split(",")) {
 			try {
 				allowed.add(Integer.parseInt(id.trim()));
--- common/mysticstones/worldgen/WorldGeneratorStones.java
+++ common/mysticstones/worldgen/WorldGeneratorStones.java
@@ -11,13 +11,13 @@ import net.minecraft.world.gen.feature.WorldGenMinable;
 import cpw.mods.fml.common.IWorldGenerator;
 
 public class WorldGeneratorStones implements IWorldGenerator {
-	public static final WorldGenMinable anorthosite = new WorldGenMinable(MysticStones.Stones.blockID, 0, 24, Block.stone.blockID);
-	public static final WorldGenMinable limestone = new WorldGenMinable(MysticStones.Stones.blockID, 2, 48, Block.stone.blockID);
-	public static final WorldGenMinable marble = new WorldGenMinable(MysticStones.Stones.blockID, 4, 24, Block.stone.blockID);
-	public static final WorldGenMinable migmatite = new WorldGenMinable(MysticStones.Stones.blockID, 6, 48, Block.stone.blockID);
-	public static final WorldGenMinable orthogneiss = new WorldGenMinable(MysticStones.Stones.blockID, 8, 48, Block.stone.blockID);
-	public static final WorldGenMinable slate = new WorldGenMinable(MysticStones.Stones.blockID, 10, 24, Block.stone.blockID);
-	public static final WorldGenMinable travertine = new WorldGenMinable(MysticStones.Stones.blockID, 12, 24, Block.stone.blockID);
+	public static final WorldGenMinable anorthosite = new WorldGenMinable(MysticStones.stones.blockID, 0, 24, Block.stone.blockID);
+	public static final WorldGenMinable limestone = new WorldGenMinable(MysticStones.stones.blockID, 2, 48, Block.stone.blockID);
+	public static final WorldGenMinable marble = new WorldGenMinable(MysticStones.stones.blockID, 4, 24, Block.stone.blockID);
+	public static final WorldGenMinable migmatite = new WorldGenMinable(MysticStones.stones.blockID, 6, 48, Block.stone.blockID);
+	public static final WorldGenMinable orthogneiss = new WorldGenMinable(MysticStones.stones.blockID, 8, 48, Block.stone.blockID);
+	public static final WorldGenMinable slate = new WorldGenMinable(MysticStones.stones.blockID, 10, 24, Block.stone.blockID);
+	public static final WorldGenMinable travertine = new WorldGenMinable(MysticStones.stones.blockID, 12, 24, Block.stone.blockID);
 
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
@@ -28,43 +28,43 @@ public class WorldGeneratorStones implements IWorldGenerator {
 
 	private void generateSurface(World world, Random random, int blockX, int blockZ) {
 		int Xcoord, Ycoord, Zcoord;
-		for (int i = 0; i < Config.Rare; i++) {
+		for (int i = 0; i < Config.rare[0]; i++) {
 			Xcoord = blockX + random.nextInt(16);
 			Ycoord = random.nextInt(40) + 20;
 			Zcoord = blockZ + random.nextInt(16);
 			anorthosite.generate(world, random, Xcoord, Ycoord, Zcoord);
 		}
-		for (int i = 0; i < Config.Rare; i++) {
+		for (int i = 0; i < Config.rare[1]; i++) {
 			Xcoord = blockX + random.nextInt(16);
 			Ycoord = random.nextInt(40) + 20;
 			Zcoord = blockZ + random.nextInt(16);
 			limestone.generate(world, random, Xcoord, Ycoord, Zcoord);
 		}
-		for (int i = 0; i < Config.Rare; i++) {
+		for (int i = 0; i < Config.rare[2]; i++) {
 			Xcoord = blockX + random.nextInt(16);
 			Ycoord = random.nextInt(40) + 20;
 			Zcoord = blockZ + random.nextInt(16);
 			marble.generate(world, random, Xcoord, Ycoord, Zcoord);
 		}
-		for (int i = 0; i < Config.Rare; i++) {
+		for (int i = 0; i < Config.rare[3]; i++) {
 			Xcoord = blockX + random.nextInt(16);
 			Ycoord = random.nextInt(40) + 20;
 			Zcoord = blockZ + random.nextInt(16);
 			migmatite.generate(world, random, Xcoord, Ycoord, Zcoord);
 		}
-		for (int i = 0; i < Config.Rare; i++) {
+		for (int i = 0; i < Config.rare[4]; i++) {
 			Xcoord = blockX + random.nextInt(16);
 			Ycoord = random.nextInt(40) + 20;
 			Zcoord = blockZ + random.nextInt(16);
 			orthogneiss.generate(world, random, Xcoord, Ycoord, Zcoord);
 		}
-		for (int i = 0; i < Config.Rare; i++) {
+		for (int i = 0; i < Config.rare[5]; i++) {
 			Xcoord = blockX + random.nextInt(16);
 			Ycoord = random.nextInt(40) + 20;
 			Zcoord = blockZ + random.nextInt(16);
 			slate.generate(world, random, Xcoord, Ycoord, Zcoord);
 		}
-		for (int i = 0; i < Config.Rare; i++) {
+		for (int i = 0; i < Config.rare[6]; i++) {
 			Xcoord = blockX + random.nextInt(16);
 			Ycoord = random.nextInt(40) + 20;
 			Zcoord = blockZ + random.nextInt(16);
