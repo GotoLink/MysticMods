b3cc647c29b31c9a10d73e61d884008c279dc00b
GotoLink <gotolinkminecraft@gmail.com>
2013-10-26 14:01:06 +0200

    Removed useless

--- common/MysticWorld/Lib/ItemIds.java
+++ common/MysticWorld/Lib/ItemIds.java
@@ -1,90 +1,35 @@
 package mysticworld.lib;
 
 public class ItemIds {
-	
-	//Default Item IDs
-	public static int STAFF_PARTS_DEFAULT = 5503;
-	
-	public static int IMBUED_SHARD_DEFAULT = 5504;
-	
-	public static int FIRE_STAFF_DEFAULT = 5505;
-	public static int WATER_STAFF_DEFAULT = 5506;
-	public static int EARTH_STAFF_DEFAULT = 5507;
-	public static int AIR_STAFF_DEFAULT = 5508;
-	public static int ENERGY_STAFF_DEFAULT = 5509;
-	
-	public static int STAFF_POWER_DEFAULT = 5510;
-	
-	public static int BUSH_FRUIT_DEFAULT = 5511;
-	
-	public static int VERDITE_HELMET_DEFAULT = 5512;
-	public static int VERDITE_CHESTPLATE_DEFAULT = 5513;
-	public static int VERDITE_LEGGINGS_DEFAULT = 5514;
-	public static int VERDITE_BOOTS_DEFAULT = 5515;
-	
-	public static int MITHRIL_HELMET_DEFAULT = 5516;
-	public static int MITHRIL_CHESTPLATE_DEFAULT = 5517;
-	public static int MITHRIL_LEGGINGS_DEFAULT = 5518;
-	public static int MITHRIL_BOOTS_DEFAULT = 5519;
-	
-	public static int ADAMANTINE_HELMET_DEFAULT = 5520;
-	public static int ADAMANTINE_CHESTPLATE_DEFAULT = 5521;
-	public static int ADAMANTINE_LEGGINGS_DEFAULT = 5522;
-	public static int ADAMANTINE_BOOTS_DEFAULT = 5523;
-	
-	public static int IRIDIUM_HELMET_DEFAULT = 5524;
-	public static int IRIDIUM_CHESTPLATE_DEFAULT = 5525;
-	public static int IRIDIUM_LEGGINGS_DEFAULT = 5526;
-	public static int IRIDIUM_BOOTS_DEFAULT = 5527;
-	
-	public static int FIRE_ORB_DEFAULT = 5528;
-	public static int WATER_ORB_DEFAULT = 5529;
-	public static int EARTH_ORB_DEFAULT = 5530;
-	public static int AIR_ORB_DEFAULT = 5531;
-	public static int ENERGY_ORB_DEFAULT = 5532;	
-	
-	public static int VOID_SHARD_DEFAULT = 5533;
-
-	//Current Item IDs
-	public static int STAFF_PARTS;
-	
-	public static int IMBUED_SHARD;
-	
-	public static int FIRE_STAFF;
-	public static int WATER_STAFF;
-	public static int EARTH_STAFF;
-	public static int AIR_STAFF;
-	public static int ENERGY_STAFF;
-	
-	public static int STAFF_POWER;
-	
-	public static int BUSH_FRUIT;
-	
-	public static int VERDITE_HELMET;
-	public static int VERDITE_CHESTPLATE;
-	public static int VERDITE_LEGGINGS;
-	public static int VERDITE_BOOTS;
-	
-	public static int MITHRIL_HELMET;
-	public static int MITHRIL_CHESTPLATE;
-	public static int MITHRIL_LEGGINGS;
-	public static int MITHRIL_BOOTS;
-	
-	public static int ADAMANTINE_HELMET;
-	public static int ADAMANTINE_CHESTPLATE;
-	public static int ADAMANTINE_LEGGINGS;
-	public static int ADAMANTINE_BOOTS;
-	
-	public static int IRIDIUM_HELMET;
-	public static int IRIDIUM_CHESTPLATE;
-	public static int IRIDIUM_LEGGINGS;
-	public static int IRIDIUM_BOOTS;
-	
-	public static int FIRE_ORB;
-	public static int WATER_ORB;
-	public static int EARTH_ORB;
-	public static int AIR_ORB;
-	public static int ENERGY_ORB;
-	
-	public static int VOID_SHARD;
+	public static int STAFF_PARTS = 5503;
+	public static int IMBUED_SHARD = 5504;
+	public static int FIRE_STAFF = 5505;
+	public static int WATER_STAFF = 5506;
+	public static int EARTH_STAFF = 5507;
+	public static int AIR_STAFF = 5508;
+	public static int ENERGY_STAFF = 5509;
+	public static int STAFF_POWER = 5510;
+	public static int BUSH_FRUIT = 5511;
+	public static int VERDITE_HELMET = 5512;
+	public static int VERDITE_CHESTPLATE = 5513;
+	public static int VERDITE_LEGGINGS = 5514;
+	public static int VERDITE_BOOTS = 5515;
+	public static int MITHRIL_HELMET = 5516;
+	public static int MITHRIL_CHESTPLATE = 5517;
+	public static int MITHRIL_LEGGINGS = 5518;
+	public static int MITHRIL_BOOTS = 5519;
+	public static int ADAMANTINE_HELMET = 5520;
+	public static int ADAMANTINE_CHESTPLATE = 5521;
+	public static int ADAMANTINE_LEGGINGS = 5522;
+	public static int ADAMANTINE_BOOTS = 5523;
+	public static int IRIDIUM_HELMET = 5524;
+	public static int IRIDIUM_CHESTPLATE = 5525;
+	public static int IRIDIUM_LEGGINGS = 5526;
+	public static int IRIDIUM_BOOTS = 5527;
+	public static int FIRE_ORB = 5528;
+	public static int WATER_ORB = 5529;
+	public static int EARTH_ORB = 5530;
+	public static int AIR_ORB = 5531;
+	public static int ENERGY_ORB = 5532;
+	public static int VOID_SHARD = 5533;
 }
