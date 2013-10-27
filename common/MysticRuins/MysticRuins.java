006de315b44a7c40ca69806b9c99addfb9d4ca8a
GotoLink <gotolinkminecraft@gmail.com>
2013-10-27 13:14:47 +0100

    Delayed config init to account for custom biomes

--- common/MysticRuins/MysticRuins.java
+++ common/MysticRuins/MysticRuins.java
@@ -1,15 +1,25 @@
 package mysticruins;
 
+import java.io.File;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = "MysticRuins", name = "Mystic Ruins", version = "0.1")
 public class MysticRuins {
+	public static File file;
+
+	@EventHandler
+	public void modsLoaded(FMLPostInitializationEvent event) {
+		Config.initialize(file);
+	}
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		Config.initialize(event.getSuggestedConfigurationFile());
+		file = event.getSuggestedConfigurationFile();
 		GameRegistry.registerWorldGenerator(new WorldGenMystic());
 	}
 }
