2c1cd82061a0b6a66d35f2fbb101122957a2af65
GotoLink <gotolinkminecraft@gmail.com>
2014-06-03 19:09:19 +0200

    Merging charge entities

--- common/MysticWorld/Entity/EntityChargeWater.java
+++ common/MysticWorld/Entity/EntityChargeWater.java
@@ -1,135 +1,34 @@
 package mysticworld.entity;
 
-import java.util.List;
-
 import mysticworld.MysticWorld;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
-public class EntityChargeWater extends Entity {
-	public EntityPlayer shootingEntity;
-	private int ticksInAir = 0;
-
+public class EntityChargeWater extends EntityCharge {
 	public EntityChargeWater(World par1World) {
 		super(par1World);
 	}
 
 	public EntityChargeWater(World par1World, EntityPlayer par2EntityPlayer) {
-		super(par1World);
-		this.setSize(0.25F, 0.25F);
-		this.setLocationAndAngles(par2EntityPlayer.posX, par2EntityPlayer.posY + par2EntityPlayer.getEyeHeight(), par2EntityPlayer.posZ, par2EntityPlayer.rotationYaw, par2EntityPlayer.rotationPitch);
-		this.setPosition(this.posX, this.posY, this.posZ);
-		this.motionX = -MathHelper.sin(this.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(this.rotationPitch / 180.0F * (float) Math.PI);
-		this.motionY = (-MathHelper.sin((this.rotationPitch) / 180.0F * (float) Math.PI));
-		this.motionZ = MathHelper.cos(this.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(this.rotationPitch / 180.0F * (float) Math.PI);
-		this.shootingEntity = par2EntityPlayer;
+		super(par1World,par2EntityPlayer);
 	}
 
 	@Override
-	public boolean canBeCollidedWith() {
-		return true;
+	public void chargeFX() {
+        MysticWorld.proxy.waterFX(this.worldObj, this.posX + (rand.nextDouble() / 2), this.posY + 0.5D, this.posZ + (rand.nextDouble() / 2), 1.0F);
 	}
 
-	@Override
-	public float getBrightness(float par1) {
-		return 1.0F;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public int getBrightnessForRender(float par1) {
-		return 15728880;
-	}
-
-	@Override
-	public float getCollisionBorderSize() {
-		return 1.0F;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public float getShadowSize() {
-		return 0.0F;
-	}
-
-	@Override
-	public void onUpdate() {
-		if (!this.worldObj.isRemote && (this.shootingEntity != null && this.shootingEntity.isDead || !this.worldObj.blockExists((int) this.posX, (int) this.posY, (int) this.posZ))) {
-			this.setDead();
-		} else {
-			super.onUpdate();
-			Vec3 vec3 = this.worldObj.getWorldVec3Pool().getVecFromPool(this.posX, this.posY, this.posZ);
-			Vec3 vec31 = this.worldObj.getWorldVec3Pool().getVecFromPool(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
-			MovingObjectPosition movingobjectposition = this.worldObj.func_147447_a(vec3, vec31, true, true, false);
-			vec3 = this.worldObj.getWorldVec3Pool().getVecFromPool(this.posX, this.posY, this.posZ);
-			vec31 = this.worldObj.getWorldVec3Pool().getVecFromPool(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
-			if (movingobjectposition != null) {
-				vec31 = this.worldObj.getWorldVec3Pool().getVecFromPool(movingobjectposition.hitVec.xCoord, movingobjectposition.hitVec.yCoord, movingobjectposition.hitVec.zCoord);
-			}
-			Entity entity = null;
-			List list = this.worldObj.getEntitiesWithinAABBExcludingEntity(this, this.boundingBox.addCoord(this.motionX, this.motionY, this.motionZ).expand(1.0D, 1.0D, 1.0D));
-			double d0 = 0.0D;
-			for (int j = 0; j < list.size(); ++j) {
-				Entity entity1 = (Entity) list.get(j);
-				if (entity1.canBeCollidedWith() && (!entity1.isEntityEqual(this.shootingEntity) || this.ticksInAir >= 25)) {
-					float f = 0.3F;
-					AxisAlignedBB axisalignedbb = entity1.boundingBox.expand(f, f, f);
-					MovingObjectPosition movingobjectposition1 = axisalignedbb.calculateIntercept(vec3, vec31);
-					if (movingobjectposition1 != null) {
-						double d1 = vec3.distanceTo(movingobjectposition1.hitVec);
-						if (d1 < d0 || d0 == 0.0D) {
-							entity = entity1;
-							d0 = d1;
-						}
-					}
-				}
-			}
-			if (entity != null) {
-				movingobjectposition = new MovingObjectPosition(entity);
-			}
-			if (movingobjectposition != null) {
-				this.onImpact(movingobjectposition);
-			}
-			this.posX += this.motionX;
-			this.posY += this.motionY;
-			this.posZ += this.motionZ;
-			float f2 = this.getMotionFactor();
-			if (this.isInWater()) {
-				for (int k = 0; k < 4; ++k) {
-					float f3 = 0.25F;
-					this.worldObj.spawnParticle("bubble", this.posX - this.motionX * f3, this.posY - this.motionY * f3, this.posZ - this.motionZ * f3, this.motionX, this.motionY, this.motionZ);
-				}
-				f2 = 0.8F;
-			}
-			MysticWorld.proxy.waterFX(this.worldObj, this.posX + (rand.nextDouble() / 2), this.posY + 0.5D, this.posZ + (rand.nextDouble() / 2), 1.0F);
-			this.setPosition(this.posX, this.posY, this.posZ);
-		}
-	}
-
-	@Override
-	protected void entityInit() {
-	}
-
-	protected float getMotionFactor() {
-		return 0.95F;
-	}
-
+    @Override
 	protected void onImpact(MovingObjectPosition par1MovingObjectPosition) {
 		if (!this.worldObj.isRemote) {
 			this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ, "random.explode", 4.0F, (1.0F + (this.worldObj.rand.nextFloat() - this.worldObj.rand.nextFloat()) * 0.2F) * 0.7F);
 			if (par1MovingObjectPosition.entityHit != null) {
 				double f3 = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
 				if (f3 > 0.0F) {
-					par1MovingObjectPosition.entityHit.addVelocity(this.motionX * 1 * 0.6000000238418579D / f3, 0.1D, this.motionZ * 1 * 0.6000000238418579D / f3);
+					par1MovingObjectPosition.entityHit.addVelocity(this.motionX * 0.6000000238418579D / f3, 0.1D, this.motionZ * 0.6000000238418579D / f3);
 				}
 				if (par1MovingObjectPosition.entityHit.isImmuneToFire()) {
 					par1MovingObjectPosition.entityHit.attackEntityFrom(DamageSource.magic, 20);
@@ -137,8 +36,8 @@ public class EntityChargeWater extends Entity {
 				if (par1MovingObjectPosition.entityHit.isBurning()) {
 					par1MovingObjectPosition.entityHit.extinguish();
 				}
-			} else {
-				int i = par1MovingObjectPosition.blockX;
+			}/* else {
+				/*int i = par1MovingObjectPosition.blockX;
 				int j = par1MovingObjectPosition.blockY;
 				int k = par1MovingObjectPosition.blockZ;
 				switch (par1MovingObjectPosition.sideHit) {
@@ -160,22 +59,8 @@ public class EntityChargeWater extends Entity {
 				case 5:
 					++i;
 				}
-			}
+			}*/
 			this.setDead();
 		}
 	}
-
-	@Override
-	protected void readEntityFromNBT(NBTTagCompound nbttagcompound) {
-	}
-
-	@Override
-	protected void writeEntityToNBT(NBTTagCompound nbttagcompound) {
-	}
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean isInRangeToRenderDist(double par1){
-        return par1 > 1.0D && super.isInRangeToRenderDist(par1);
-    }
 }
